8ebb9aa2d592366c11794ae5d0f540579359588e
HelenaOliveira1 <helenna54319@gmail.com>
2016-08-30 13:05:26 -0300

    Questão 20

---
+++ Questão 20.py
@@ -0,0 +1,23 @@
+print("Questão 20")
+print("")
+
+vzs = int(input("Quantas vezes você quer calcular o fatorial? "))
+print("")
+num = int(input("Digite um número para o fatorial: "))
+fatorial = 1
+
+for i in range (vzs):
+    if (num < 16):
+        while(num > 1):
+            fatorial = fatorial*num
+            num -= 1
+            
+        print("O fatorial do número é:", fatorial)
+        
+    else:
+        print("Número não suportado!")
+        
+    fatorial = 1
+    if (i < (vzs - 1)):
+        num = int(input("Digite um número para o fatorial: "))
+

f6e86ce5cd077bf0c5fd18c0b93b550e3bfa6927
HelenaOliveira1 <helenna54319@gmail.com>
2016-08-30 12:38:15 -0300

    Questão 19

---
+++ Questão 19.py
@@ -0,0 +1,26 @@
+N = int(input("Quantos números você quer que sejam comparados? "))
+print("")
+aux = 1
+num = int(input("Digite um número: "))
+MA = num
+ME = num
+soma = 0
+
+if ((num <= 1000 and num > 1) or num == 1):
+    while (aux <= N):
+        if (aux < N):
+           num = int(input("Digite um número: "))
+        if (num > MA):
+            MA = num
+        elif (num < ME):
+            ME = num
+        soma = soma + num
+        aux += 1
+    print("Maior número:", MA)
+    print("Menor número:", ME)
+    print("Soma igual a:", soma)
+
+else:
+    print("Números Grandes Demais.")
+
+

6737da3081b6b7ba24a18ae1659d1b0dfe9f4a57
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 16:42:40 -0300

    Questão 35

---
+++ Questão 35.py
@@ -0,0 +1,15 @@
+print("Questão 35")
+print("")
+
+numero = int(input("Digite um número: "))
+print("")
+aux = 1
+p = 0
+np = 0
+print("Números existentes de 1 até", numero)
+while (aux <= numero):
+  result = numero / aux
+  if (result == int(result)):
+    print(aux)      
+  aux += 1
+  

bb07ce6fe80b9444d99eca263a46512efcd8ca73
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 15:11:56 -0300

    Questão 31

---
+++ Questão 31.py
@@ -0,0 +1,19 @@
+print("Questão 31")
+print("")
+
+valor = float(input("Produto: "))
+total = 0
+
+while (valor != 0):
+    total += valor
+    valor = float(input("Produto: "))
+    
+print("Valor total: ", total)
+dinheiro = float(input("Dinheiro: "))
+troco = dinheiro - total
+print("Troco: ", troco)
+print("...")
+
+    
+    
+                  

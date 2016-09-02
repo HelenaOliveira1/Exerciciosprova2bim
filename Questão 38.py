6fca089422f557e9e6970243cdd019d10ffdb067
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 19:26:45 -0300

    Questão 38

--- Questão 38.py
+++ Questão 38.py
@@ -4,31 +4,30 @@ print("")
 print("Parte I - Salário inicial 1.000,00 - Salário Atual (2016)")
 print("")
 
-ano = 1995
-salario = 1.015
-percentual = 1.5
+ano = 1996
+salario = 1015
+percentual = 0.015
 
-while (ano+2 <= 2016):
-    aumento = salario*(percentual/100)
-    salario += aumento
-    percentual *= 2
+while (ano <= 2016):
     ano += 1
-
-print("Salário atual: %.2f" %salario)
-
-print("")
-print("Parte II - Salário inicial informado pelo usuário - Salário Atual (2016)")
-print("")
-
-ano = 1995
-salario = float(input("Digite o valor do salário inicial: "))
-salario += 15
-percentual = 1.5
-
-while (ano+2 <= 2016):
-    aumento = salario*(percentual/100)
+    aumento = salario*percentual
     salario += aumento
-    percentual *= 2
-    ano += 1
+    percentual = percentual*2
 
-print("Salário atual: %.2f" %salario)
+print("Salário Atual: ", salario)
+
+("")
+print("Parte II - Salário inicial fornecido pelo usuário - Salário Atual (2016)")
+print("")
+
+ano = 1996
+salario = float(input("Digite seu salário inicial: "))
+percentual = 0.015
+
+while (ano <= 2016):
+    ano += 1
+    aumento = salario*percentual
+    salario += aumento
+    percentual = percentual*2
+
+print("Salário Atual: ", salario)

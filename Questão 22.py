8f3f1772706199f27ae81220b6ecab2d88e7924a
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 16:16:35 -0300

    Questão 22

--- Questão 22.py
+++ Questão 22.py
@@ -3,11 +3,20 @@ print("")
 
 n = int(input("Digite um número: "))
 aux = 1
+p = 0
+np = 0
 while (aux <= n):
   result = n / aux
   if (result == int(result)):
-    print("É divisível por", aux)     
-  aux = aux + 1
+    print("É divisível por", aux)
+    p += 1
+  else:
+    np += 1
+  aux += 1
+if (p == 2):
+  print("")
+  print("É primo!")
+else:
+  print("")
+  print("Não é primo!")
     
-print("")
-print("OBS.: Quando o número é divísivel apenas por 1 e por ele mesmo É PRIMO")

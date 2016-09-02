e8f2beabf7b6e4211232eb294e7ed02d8d36e378
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 16:09:42 -0300

    Questão 21

--- Questão 21.py
+++ Questão 21.py
@@ -1,15 +1,21 @@
 print("Questão 21")
 print("")
 
-n = int(input("Digite um número: "))
+N = int(input("Digite um número: "))
 aux = 1
-while (aux <= n):
-	result = n / aux
-	if (result == int(result)):
-		print(aux, "É primo")
-	else:
-		print(aux, "Não é primo")
-	aux = aux + 1
+primo = 0
+naoprimo = 0
 
-print("")
-print("OBS.: Se aparecer na tela a mensagem -É primo- apenas duas vezes, significa que o número é realmente primo, senão significa que ele não é primo")
+while (aux < N+1):
+    result = N / aux
+    if (result == int(result)):
+      primo +=1
+    else:
+      naoprimo += 1
+    aux += 1
+
+if (primo == 2):
+    print("É primo!")
+else:
+    print("Não é primo!")
+ 	

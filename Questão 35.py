0798e3d1218008a20a96106b7a1e58524f65263c
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 16:45:33 -0300

    Questão 35

--- Questão 35.py
+++ Questão 35.py
@@ -7,7 +7,7 @@ aux = 1
 p = 0
 np = 0
 print("Números existentes de 1 até", numero)
-while (aux <= numero):
+while (aux < numero):
   result = numero / aux
   if (result == int(result)):
     print(aux)      

03b9ee1e1a3a551dce806a08bbb4fbde82a34938
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 14:37:54 -0300

    Questão 29

---
+++ Questão 29.py
@@ -0,0 +1,11 @@
+print("Questão 29")
+print("")
+
+preco = 1.99
+aux = 1
+
+print("Loja Quase Dois - Tabela de Preços")
+for tabela in range (1,51):
+    print(aux,"- R$ %.2f" %preco)
+    aux +=1
+    preco += 1.99

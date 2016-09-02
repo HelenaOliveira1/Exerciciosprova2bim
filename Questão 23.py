4677289473587a7a85c994f5cfcb97ae746bb27c
HelenaOliveira1 <helenna54319@gmail.com>
2016-09-02 16:32:37 -0300

    Questão 23

--- Questão 23.py
+++ Questão 23.py
@@ -3,8 +3,15 @@ print("")
 
 N = int(input("Digite um número: "))
 aux = 1
-
-while (aux <= N):
+p = 0
+np = 0
+while (aux < N):
+  result = N / aux
+  if (result == int(result)):
+    print(aux)      
+  aux += 1
+  
+print("Número de divisões executadas: ", aux)
    
     
 
